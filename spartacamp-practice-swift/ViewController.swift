b7d6b540484a2d4bf6e77c7c842732693496ef17
kbanchi <b.keisuke1111@gmail.com>
2021-08-15 14:20:02 +0900

    Initial Commit

---
+++ spartacamp-practice-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  spartacamp-practice-swift
+//
+//  Created by 伴地慶介 on 2021/08/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
